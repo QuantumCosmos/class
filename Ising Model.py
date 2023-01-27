bb88e83d98badc39b4b20391b83c4a79e0ebe426
Saptarshi Dutta <quantumcosmos7@gmail.com>
2023-01-27 22:24:04 +0530

    all plots added

--- Ising Model.py
+++ Ising Model.py
@@ -1,11 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
-dim = n = 10 #int(input())
-S = np.random.choice([1, -1],size=(n,n)) # np.full((dim, dim), 1)
+dim = n = 10  # int(input())
+S = np.random.choice([1, -1], size=(n, n))  # np.full((dim, dim), 1)
 # S = np.full((dim, dim), 1)
 N = 1000
 K = 1
-
 
 
 def total(S):
@@ -13,9 +12,12 @@ def total(S):
     E_p = 0
     for i in range(dim):
         for j in range(dim):
-            E_p -= J*S[i, j]*(S[(i+1)%n,j]+S[(i-1)%n,j]+S[i,(j-1)%n]+S[i,(j+1)%n])
+            E_p -= J*S[i, j]*(S[(i+1) % n, j]+S[(i-1) %
+                              n, j]+S[i, (j-1) % n]+S[i, (j+1) % n])
 
     return E_p/4
+
+
 def change(T, S):
     # total_E = []
     # inst_avg_E = []
@@ -29,7 +31,8 @@ def change(T, S):
             # E_prev = total(S)
             s = S[i][j]
             # E_next = total(S)
-            delta = 2*s*(S[(i+1)%n,j]+S[(i-1)%n,j]+S[i,(j-1)%n]+S[i,(j+1)%n])
+            delta = 2*s*(S[(i+1) % n, j]+S[(i-1) % n, j] +
+                         S[i, (j-1) % n]+S[i, (j+1) % n])
             prob = np.exp(-delta/(K*T))
             r_p = np.random.rand(1)[0]
             # print(prob)
@@ -40,7 +43,8 @@ def change(T, S):
                 # E_prev = E_next
             # else:
             #     S[i][j] = -S[i][j]
-        
+
+
     # total_E.append(E_next)
     # avg_E = np.average(total_E)100*N*N)
     # return avg_E
@@ -49,24 +53,39 @@ def change(T, S):
 # print("Last ACCEPTED config")
 # print(S)
 # print(E_prev)
-T = 1
+Ti = 1.54
+Tf = 3.28
 x = 5000*100
+x2 = x*5000
 for _ in range(1000):
-    change(T, S)
-avg_E_list = np.zeros(20)
-T = np.linspace(T,T+2,20)
+    change(Ti, S)
+nt = 32
+avg_E_list = np.zeros(nt)
+avg_M_list = np.zeros(nt)
+avg_E2_list = np.zeros(nt)
+avg_M2_list = np.zeros(nt)
+T = np.linspace(Ti, Tf, nt)
 
 # E = 0
-for t in range(20):
+for t in range(nt):
     print(t)
-    E = 0
+    E = M = E2 = M2 = 0
+    for _ in range(1000):
+        change(T[t], S)
     for i in range(5000):
         change(T[t], S)
         e = total(S)
-        E +=  e
+        m = np.sum(S)
+        E += e
+        M += m
+        E2 += e*e
+        M2 += m*m
     # print(S)
     # print(e, t)
     avg_E_list[t] = E/x
+    avg_M_list[t] = M/(x2)
+    avg_E2_list[t] = (E2/x - E*E/x2)/(T[t]*T[t])
+    avg_M2_list[t] = (M2/x - M*M/x2)/T[t]
 
 # iter = range(1, len(avg_E_list)+1)
 # a = np.cumsum(avg_E_list)/iter
@@ -77,6 +96,16 @@ for t in range(20):
 # for t in T:
 #     S_T = S.copy()
 #     avg_E_list.append(change(S_T, t))
+fig, ax = plt.subplots(2, 2)
 
-plt.plot(T, avg_E_list, 'o')
-plt.show()
+ax[0, 0].plot(T, avg_E_list, 'o')  # row=0, col=0
+ax[1, 0].plot(T, avg_M_list, 'o')  # row=1, col=0
+ax[0, 1].plot(T, avg_E2_list, 'o')  # row=0, col=1
+ax[1, 1].plot(T, avg_M2_list, 'o')  # row=1, col=1
+# plt.plot(T, avg_E_list, 'o')
+ax[0, 0].set_xlabel("Energy")
+ax[1, 0].set_xlabel("Magnetization")
+ax[0, 1].set_xlabel("Specific Heat")
+ax[1, 1].set_xlabel("Susceptibility")
+# plt.plot(T, avg_M2_list)
+# plt.show()
---
+++ im.py
@@ -0,0 +1,8 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+#Generating a Configuration with random spins
+config = np.random.choice([-1, 1], size=(10, 10))
+print(config)
+config np.random. choice([-1, 1], size=(200, 200))
+geromarray(np.uint8 ( (config + 1) * 0.5 * 255
